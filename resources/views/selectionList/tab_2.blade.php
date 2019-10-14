8f1bee1f3ef0932187d631f40e1f5ee5f22d73d7
sjlouji <sjlouji10@gmail.com>
2019-10-14 13:01:59 +0000

    test branch j

---
+++ resources/views/selectionList/tab_2.blade.php
@@ -0,0 +1,42 @@
+<section class="content">
+    <div class="row">
+            <div class="col-xs-12">
+            
+                    <div class="box-header">
+                        <h3 class="box-title">Departments</h3>
+                    </div>
+                    <div class="box-body">
+                        <table id="example1" class="table table-bordered table-striped ">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Department Name</th>
+                                    <th>Management Quota</th>
+                                    <th>Open Catholic</th>
+                                    <th>Roman Catholic</th>
+                                    <th>Dalit Catholic</th>
+                                    <th>Rural / Poor</th>
+                                </tr>
+                                </thead>
+                                    
+                                <tfoot>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Department Name</th>
+                                    <th>Management Quota</th>
+                                    <th>Open Catholic</th>
+                                    <th>Roman Catholic</th>
+                                    <th>Dalit Catholic</th>
+                                    <th>Rural / Poor</th>
+
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+            </div>
+    </div>
+</section>
+
+@section('scripts')
+
+@stop
